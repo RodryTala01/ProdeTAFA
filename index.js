e005f523fdc19a61593fa8068b669ac0a7620d8a
RodryTala01 <rodrytalarico@gmail.com>
2023-06-19 03:24:13 -0300

    add

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-fecha32avos = document.getElementById("fecha32avos");
-fecha1 = document.getElementById("fecha1");
-textoCaja = document.getElementById("textoCaja");
-cajaBotonesFechas = document.getElementById("cajaBotonesFechas");
 
 function mostrarFecha1() {
+    fecha32avos = document.getElementById("fecha32avos");
+    fecha1 = document.getElementById("fecha1");
+    textoCaja = document.getElementById("textoCaja");
+    cajaBotonesFechas = document.getElementById("cajaBotonesFechas");
+
+
     fecha32avos.style.display = "none";
     fecha1.style.display = "block";
 }
 
 function mostrarFecha32avos() {
+    fecha32avos = document.getElementById("fecha32avos");
+    fecha1 = document.getElementById("fecha1");
+    textoCaja = document.getElementById("textoCaja");
+    cajaBotonesFechas = document.getElementById("cajaBotonesFechas");
+
+
     fecha32avos.style.display = "block";
     fecha1.style.display = "none";
 }
