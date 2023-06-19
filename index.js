5a61faf7c74406d8e9574791fe18e228d25ebb38
RodryTala01 <rodrytalarico@gmail.com>
2023-06-19 03:01:21 -0300

    navegacion

---
+++ index.js
@@ -0,0 +1,10 @@
+function mostrarFecha1(){
+    fecha32avos = document.getElementById("fecha32avos");
+    fecha1 = document.getElementById("fecha1");
+    textoCaja = document.getElementById("textoCaja")
+
+    fecha32avos.style.display = "none";
+    fecha1.style.display = "block";
+
+    textoCaja.innerHTML = "FECHA 1"
+}
