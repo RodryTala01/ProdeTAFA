6683ddf5e96a3eb7ca19d9b31db0dbfe35ae41ea
RodryTala01 <rodrytalarico@gmail.com>
2024-02-27 05:46:44 -0300

    cargando

--- JS/leer.js
+++ JS/leer.js
@@ -1,7 +1,8 @@
 const vue = new Vue({
     el: '#app',
     data: {
-        listaDatos: []
+        listaDatos: [],
+        loaded: false
     },
     created() {
         this.getLista();
@@ -15,21 +16,36 @@ const vue = new Vue({
         },
         getLista() {
             // id de la hoja de calculo
-            idSheets = '169rYxgQwzjatwq4iFgCTPn8y83IdvRH-Guy2PmTtnRk';
+            const idSheets = '169rYxgQwzjatwq4iFgCTPn8y83IdvRH-Guy2PmTtnRk';
             //// nuestra      APIKey
-            apiKey = 'AIzaSyDS9VtkbPnvgTil44LtEiQ--DLIp5-GE2g';
+            const apiKey = 'AIzaSyDS9VtkbPnvgTil44LtEiQ--DLIp5-GE2g';
             // rango de la hoja de calculo que queremos leer
-            values = 'A2:AE12';
+            const values = 'A2:AE12';
             // fetch es un método nativo para hacer peticiones http
             // en el navegador 
             fetch("https://content-sheets.googleapis.com/v4/spreadsheets/" + idSheets + "/values/A2:AZ100?access_token=" + apiKey + "&key=" + apiKey)
                 .then((lista) => {
                     return lista.json()
                 }).then((valores) => {
-                    this.listaDatos = valores.values
+                    this.listaDatos = valores.values;
+                    // Establecer loaded como true una vez que los datos estén cargados
+                    ocultarPelotaYTextoDeCarga()
                 }).catch(err => {
                     console.log(err);
                 })
         } // fin funcion getLista()
     } // fin methods
-}) // fin instancia
+});
+
+function ocultarPelotaYTextoDeCarga() {
+    var containerPelota = document.getElementById("containerPelota");
+    var textoCargando = document.getElementById("textoCargando");
+
+
+    if (containerPelota && textoCargando) {
+        containerPelota.style.opacity = "0";
+        textoCargando.style.opacity = "0";
+    }
+
+    document.getElementById("app").style.display = "block";
+}
