138547f8f2bb59f310022e9109c2a3370157cec2
RodryTala01 <rodrytalarico@gmail.com>
2024-02-28 03:20:07 -0300

    Calcular puntos en todas las fechas

--- JS/indexProde.js
+++ JS/indexProde.js
@@ -194,6 +194,499 @@ function noHayFecha() {
 }
 
 
+function abrirInput32avos() {
+    document.getElementById("campoInput32avos").classList.remove("hidden");
+    document.getElementById("btnAbrirInput32avos").classList.add("hidden");
+}
+
+function volver32avos() {
+    document.getElementById("campoInput32avos").classList.add("hidden");
+    document.getElementById("btnAbrirInput32avos").classList.remove("hidden");
+}
+
+function ajustarAltura32avos() {
+    var textarea = document.getElementById("inputTexto32avos");
+    textarea.style.height = "auto";
+    textarea.style.height = textarea.scrollHeight + "px";
+}
+
+function corroborar32avos() {
+
+    var texto = document.getElementById("inputTexto32avos").value;
+    plenos = 0;
+    parciales = 0;
+    errores = 0;
+    puntosTotales = 0;
+
+    const numeros = texto.match(/\d+/g);
+    if (!numeros) return [];
+
+    const pares = [];
+    for (let i = 0; i < numeros.length; i += 2) {
+        if (i + 1 < numeros.length) {
+            pares.push([parseInt(numeros[i]), parseInt(numeros[i + 1])]);
+        }
+    }
+
+    const fechaCorrecta = [
+        [0, 0],
+        [4, 1],
+        [2, 0],
+        [1, 3],
+        [2, 4],
+        [4, 0],
+        [0, 0],
+        [1, 2],
+        [1, 2],
+        [1, 0],
+        [1, 0],
+        [1, 2]
+    ];
+
+    const fechaSinX = fechaCorrecta.filter(item => item[0] !== "X" && item[1] !== "X");
+    const error32avos = document.getElementById("error32avos");
+    if (pares.length < fechaSinX.length) {
+        error32avos.innerHTML = "ERROR: Te faltan partidos.";
+    } else {
+        error32avos.innerHTML = "";
+    }
+
+    for (let i = 0; i < 12; i++) {
+        if ((isNaN(fechaCorrecta[i][0]) || isNaN(fechaCorrecta[i][1])) || (isNaN(pares[i][0]) || isNaN(pares[i][1]))) {
+            continue; // Si uno de los valores es "X" o no es un número, continuar con la siguiente iteración
+        }
+
+        if (pares[i][0] == fechaCorrecta[i][0] && pares[i][1] == fechaCorrecta[i][1]) {
+            plenos++;
+        } else if ((pares[i][0] > pares[i][1] && fechaCorrecta[i][0] > fechaCorrecta[i][1]) || (pares[i][0] < pares[i][1] && fechaCorrecta[i][0] < fechaCorrecta[i][1])
+            || (pares[i][0] == pares[i][1] && fechaCorrecta[i][0] == fechaCorrecta[i][1] && pares[i][0] !== fechaCorrecta[i][0] && pares[i][1] !== fechaCorrecta[i][1])) {
+            parciales++;
+        } else {
+            errores++;
+        }
+    }
+    puntosTotales = 3 * plenos + 1 * parciales + penales32avos;
+
+    var puntosTotalesTexto = puntosTotales === 1 ? 'PUNTITO' : 'PUNTOS';
+    var plenosTexto = plenos === 1 ? 'PLENO' : 'PLENOS';
+    var parcialesTexto = parciales === 1 ? 'PARCIAL' : 'PARCIALES';
+    var erroresTexto = errores === 1 ? 'ERROR' : 'ERRORES';
+    var penalesTexto = penales32avos === 1 ? 'PENAL' : 'PENALES';
+
+    Swal.fire({
+        title: '<span style="color:black">' + puntosTotales + ' ' + puntosTotalesTexto + '</span>',
+        html: '<span style="color:green">' + plenos + ' ' + plenosTexto + '</span><br>' +
+            '<span style="color:rgb(131, 131, 2)">' + parciales + ' ' + parcialesTexto + '</span><br>' +
+            '<span style="color:red">' + errores + ' ' + erroresTexto + '</span><br>' +
+            '<span style="color:orange">' + penales32avos + ' ' + penalesTexto + '</span>',
+        confirmButtonText: 'OK'
+    });
+
+
+}
+
+var penales32avos = 0;
+
+function sumarPenales32avos() {
+    penales32avos++;
+    actualizarPenales32avos();
+}
+
+function restarPenales32avos() {
+    if (penales32avos > 0) {
+        penales32avos--;
+        actualizarPenales32avos();
+    } else {
+        alert("El número no puede ser menor que 0 pedazo de autista");
+    }
+}
+
+function actualizarPenales32avos() {
+    document.getElementById("penales32avos").innerText = penales32avos;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function abrirInputFecha1() {
+    document.getElementById("campoInputFecha1").classList.remove("hidden");
+    document.getElementById("btnAbrirInputFecha1").classList.add("hidden");
+}
+
+function volverFecha1() {
+    document.getElementById("campoInputFecha1").classList.add("hidden");
+    document.getElementById("btnAbrirInputFecha1").classList.remove("hidden");
+}
+
+function ajustarAlturaFecha1() {
+    var textarea = document.getElementById("inputTextoFecha1");
+    textarea.style.height = "auto";
+    textarea.style.height = textarea.scrollHeight + "px";
+}
+
+function corroborarFecha1() {
+
+    var texto = document.getElementById("inputTextoFecha1").value;
+    plenos = 0;
+    parciales = 0;
+    errores = 0;
+    puntosTotales = 0;
+
+    const numeros = texto.match(/\d+/g);
+    if (!numeros) return [];
+
+    const pares = [];
+    for (let i = 0; i < numeros.length; i += 2) {
+        if (i + 1 < numeros.length) {
+            pares.push([parseInt(numeros[i]), parseInt(numeros[i + 1])]);
+        }
+    }
+
+    const fechaCorrecta = [
+        [1, 3],
+        [0, 4],
+        [1, 3],
+        [0, 1],
+        [1, 1],
+        [1, 0],
+        [1, 0],
+        [2, 0],
+        [0, 0],
+        [1, 1],
+        [1, 1],
+        [1, 1]
+    ];
+
+    const fechaSinX = fechaCorrecta.filter(item => item[0] !== "X" && item[1] !== "X");
+    const errorFecha1 = document.getElementById("errorFecha1");
+    if (pares.length < fechaSinX.length) {
+        errorFecha1.innerHTML = "ERROR: Te faltan partidos.";
+    } else {
+        errorFecha1.innerHTML = "";
+    }
+
+    for (let i = 0; i < 12; i++) {
+        if ((isNaN(fechaCorrecta[i][0]) || isNaN(fechaCorrecta[i][1])) || (isNaN(pares[i][0]) || isNaN(pares[i][1]))) {
+            continue; // Si uno de los valores es "X" o no es un número, continuar con la siguiente iteración
+        }
+
+        if (pares[i][0] == fechaCorrecta[i][0] && pares[i][1] == fechaCorrecta[i][1]) {
+            plenos++;
+        } else if ((pares[i][0] > pares[i][1] && fechaCorrecta[i][0] > fechaCorrecta[i][1]) || (pares[i][0] < pares[i][1] && fechaCorrecta[i][0] < fechaCorrecta[i][1])
+            || (pares[i][0] == pares[i][1] && fechaCorrecta[i][0] == fechaCorrecta[i][1] && pares[i][0] !== fechaCorrecta[i][0] && pares[i][1] !== fechaCorrecta[i][1])) {
+            parciales++;
+        } else {
+            errores++;
+        }
+    }
+    puntosTotales = 3 * plenos + 1 * parciales + penalesFecha1;
+
+    var puntosTotalesTexto = puntosTotales === 1 ? 'PUNTITO' : 'PUNTOS';
+    var plenosTexto = plenos === 1 ? 'PLENO' : 'PLENOS';
+    var parcialesTexto = parciales === 1 ? 'PARCIAL' : 'PARCIALES';
+    var erroresTexto = errores === 1 ? 'ERROR' : 'ERRORES';
+    var penalesTexto = penalesFecha1 === 1 ? 'PENAL' : 'PENALES';
+
+    Swal.fire({
+        title: '<span style="color:black">' + puntosTotales + ' ' + puntosTotalesTexto + '</span>',
+        html: '<span style="color:green">' + plenos + ' ' + plenosTexto + '</span><br>' +
+            '<span style="color:rgb(131, 131, 2)">' + parciales + ' ' + parcialesTexto + '</span><br>' +
+            '<span style="color:red">' + errores + ' ' + erroresTexto + '</span><br>' +
+            '<span style="color:orange">' + penalesFecha1 + ' ' + penalesTexto + '</span>',
+        confirmButtonText: 'OK'
+    });
+
+
+}
+
+var penalesFecha1 = 0;
+
+function sumarPenalesFecha1() {
+    penalesFecha1++;
+    actualizarPenalesFecha1();
+}
+
+function restarPenalesFecha1() {
+    if (penalesFecha1 > 0) {
+        penalesFecha1--;
+        actualizarPenalesFecha1();
+    } else {
+        alert("El número no puede ser menor que 0 pedazo de autista");
+    }
+}
+
+function actualizarPenalesFecha1() {
+    document.getElementById("penalesFecha1").innerText = penalesFecha1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function abrirInput16avos() {
+    document.getElementById("campoInput16avos").classList.remove("hidden");
+    document.getElementById("btnAbrirInput16avos").classList.add("hidden");
+}
+
+function volver16avos() {
+    document.getElementById("campoInput16avos").classList.add("hidden");
+    document.getElementById("btnAbrirInput16avos").classList.remove("hidden");
+}
+
+function ajustarAltura16avos() {
+    var textarea = document.getElementById("inputTexto16avos");
+    textarea.style.height = "auto";
+    textarea.style.height = textarea.scrollHeight + "px";
+}
+
+function corroborar16avos() {
+
+    var texto = document.getElementById("inputTexto16avos").value;
+    plenos = 0;
+    parciales = 0;
+    errores = 0;
+    puntosTotales = 0;
+
+    const numeros = texto.match(/\d+/g);
+    if (!numeros) return [];
+
+    const pares = [];
+    for (let i = 0; i < numeros.length; i += 2) {
+        if (i + 1 < numeros.length) {
+            pares.push([parseInt(numeros[i]), parseInt(numeros[i + 1])]);
+        }
+    }
+
+    const fechaCorrecta = [
+        [3, 2],
+        [1, 1],
+        [1, 2],
+        [1, 1],
+        [0, 2],
+        [0, 0],
+        [1, 2],
+        [1, 1],
+        [2, 1],
+        [1, 0],
+        [2, 1],
+        [2, 1]
+    ];
+
+    const fechaSinX = fechaCorrecta.filter(item => item[0] !== "X" && item[1] !== "X");
+    const error16avos = document.getElementById("error16avos");
+    if (pares.length < fechaSinX.length) {
+        error16avos.innerHTML = "ERROR: Te faltan partidos.";
+    } else {
+        error16avos.innerHTML = "";
+    }
+
+    for (let i = 0; i < 12; i++) {
+        if ((isNaN(fechaCorrecta[i][0]) || isNaN(fechaCorrecta[i][1])) || (isNaN(pares[i][0]) || isNaN(pares[i][1]))) {
+            continue; // Si uno de los valores es "X" o no es un número, continuar con la siguiente iteración
+        }
+
+        if (pares[i][0] == fechaCorrecta[i][0] && pares[i][1] == fechaCorrecta[i][1]) {
+            plenos++;
+        } else if ((pares[i][0] > pares[i][1] && fechaCorrecta[i][0] > fechaCorrecta[i][1]) || (pares[i][0] < pares[i][1] && fechaCorrecta[i][0] < fechaCorrecta[i][1])
+            || (pares[i][0] == pares[i][1] && fechaCorrecta[i][0] == fechaCorrecta[i][1] && pares[i][0] !== fechaCorrecta[i][0] && pares[i][1] !== fechaCorrecta[i][1])) {
+            parciales++;
+        } else {
+            errores++;
+        }
+    }
+    puntosTotales = 3 * plenos + 1 * parciales + penales16avos;
+
+    var puntosTotalesTexto = puntosTotales === 1 ? 'PUNTITO' : 'PUNTOS';
+    var plenosTexto = plenos === 1 ? 'PLENO' : 'PLENOS';
+    var parcialesTexto = parciales === 1 ? 'PARCIAL' : 'PARCIALES';
+    var erroresTexto = errores === 1 ? 'ERROR' : 'ERRORES';
+    var penalesTexto = penales16avos === 1 ? 'PENAL' : 'PENALES';
+
+    Swal.fire({
+        title: '<span style="color:black">' + puntosTotales + ' ' + puntosTotalesTexto + '</span>',
+        html: '<span style="color:green">' + plenos + ' ' + plenosTexto + '</span><br>' +
+            '<span style="color:rgb(131, 131, 2)">' + parciales + ' ' + parcialesTexto + '</span><br>' +
+            '<span style="color:red">' + errores + ' ' + erroresTexto + '</span><br>' +
+            '<span style="color:orange">' + penales16avos + ' ' + penalesTexto + '</span>',
+        confirmButtonText: 'OK'
+    });
+
+
+}
+
+var penales16avos = 0;
+
+function sumarPenales16avos() {
+    penales16avos++;
+    actualizarPenales16avos();
+}
+
+function restarPenales16avos() {
+    if (penales16avos > 0) {
+        penales16avos--;
+        actualizarPenales16avos();
+    } else {
+        alert("El número no puede ser menor que 0 pedazo de autista");
+    }
+}
+
+function actualizarPenales16avos() {
+    document.getElementById("penales16avos").innerText = penales16avos;
+}
+
+
+
+
+
+
+
+
+
+
+
+function abrirInputFecha2() {
+    document.getElementById("campoInputFecha2").classList.remove("hidden");
+    document.getElementById("btnAbrirInputFecha2").classList.add("hidden");
+}
+
+function volverFecha2() {
+    document.getElementById("campoInputFecha2").classList.add("hidden");
+    document.getElementById("btnAbrirInputFecha2").classList.remove("hidden");
+}
+
+function ajustarAlturaFecha2() {
+    var textarea = document.getElementById("inputTextoFecha2");
+    textarea.style.height = "auto";
+    textarea.style.height = textarea.scrollHeight + "px";
+}
+
+function corroborarFecha2() {
+
+    var texto = document.getElementById("inputTextoFecha2").value;
+    plenos = 0;
+    parciales = 0;
+    errores = 0;
+    puntosTotales = 0;
+
+    const numeros = texto.match(/\d+/g);
+    if (!numeros) return [];
+
+    const pares = [];
+    for (let i = 0; i < numeros.length; i += 2) {
+        if (i + 1 < numeros.length) {
+            pares.push([parseInt(numeros[i]), parseInt(numeros[i + 1])]);
+        }
+    }
+
+    const fechaCorrecta = [
+        [1, 1],
+        [2, 0],
+        [1, 0],
+        [1, 1],
+        [1, 1],
+        [1, 0],
+        [1, 0],
+        [1, 1],
+        [0, 0],
+        [1, 1],
+        [3, 1],
+        [0, 1]
+    ];
+
+    const fechaSinX = fechaCorrecta.filter(item => item[0] !== "X" && item[1] !== "X");
+    const errorFecha2 = document.getElementById("errorFecha2");
+    if (pares.length < fechaSinX.length) {
+        errorFecha2.innerHTML = "ERROR: Te faltan partidos.";
+    } else {
+        errorFecha2.innerHTML = "";
+    }
+
+    for (let i = 0; i < 12; i++) {
+        if ((isNaN(fechaCorrecta[i][0]) || isNaN(fechaCorrecta[i][1])) || (isNaN(pares[i][0]) || isNaN(pares[i][1]))) {
+            continue; // Si uno de los valores es "X" o no es un número, continuar con la siguiente iteración
+        }
+
+        if (pares[i][0] == fechaCorrecta[i][0] && pares[i][1] == fechaCorrecta[i][1]) {
+            plenos++;
+        } else if ((pares[i][0] > pares[i][1] && fechaCorrecta[i][0] > fechaCorrecta[i][1]) || (pares[i][0] < pares[i][1] && fechaCorrecta[i][0] < fechaCorrecta[i][1])
+            || (pares[i][0] == pares[i][1] && fechaCorrecta[i][0] == fechaCorrecta[i][1] && pares[i][0] !== fechaCorrecta[i][0] && pares[i][1] !== fechaCorrecta[i][1])) {
+            parciales++;
+        } else {
+            errores++;
+        }
+    }
+    puntosTotales = 3 * plenos + 1 * parciales + penalesFecha2;
+
+    var puntosTotalesTexto = puntosTotales === 1 ? 'PUNTITO' : 'PUNTOS';
+    var plenosTexto = plenos === 1 ? 'PLENO' : 'PLENOS';
+    var parcialesTexto = parciales === 1 ? 'PARCIAL' : 'PARCIALES';
+    var erroresTexto = errores === 1 ? 'ERROR' : 'ERRORES';
+    var penalesTexto = penalesFecha2 === 1 ? 'PENAL' : 'PENALES';
+
+    Swal.fire({
+        title: '<span style="color:black">' + puntosTotales + ' ' + puntosTotalesTexto + '</span>',
+        html: '<span style="color:green">' + plenos + ' ' + plenosTexto + '</span><br>' +
+            '<span style="color:rgb(131, 131, 2)">' + parciales + ' ' + parcialesTexto + '</span><br>' +
+            '<span style="color:red">' + errores + ' ' + erroresTexto + '</span><br>' +
+            '<span style="color:orange">' + penalesFecha2 + ' ' + penalesTexto + '</span>',
+        confirmButtonText: 'OK'
+    });
+
+
+}
+
+var penalesFecha2 = 0;
+
+function sumarPenalesFecha2() {
+    penalesFecha2++;
+    actualizarPenalesFecha2();
+}
+
+function restarPenalesFecha2() {
+    if (penalesFecha2 > 0) {
+        penalesFecha2--;
+        actualizarPenalesFecha2();
+    } else {
+        alert("El número no puede ser menor que 0 pedazo de autista");
+    }
+}
+
+function actualizarPenalesFecha2() {
+    document.getElementById("penalesFecha2").innerText = penalesFecha2;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 function abrirInputOctavos() {
     document.getElementById("campoInputOctavos").classList.remove("hidden");
     document.getElementById("btnAbrirInputOctavos").classList.add("hidden");
@@ -242,6 +735,14 @@ function corroborarFechaOctavos() {
         [1, 0],
         [1, 1]
     ];
+
+    const fechaSinX = fechaCorrecta.filter(item => item[0] !== "X" && item[1] !== "X");
+    const errorOctavos = document.getElementById("errorOctavos");
+    if (pares.length < fechaSinX.length) {
+        errorOctavos.innerHTML = "ERROR: Te faltan partidos.";
+    } else {
+        errorOctavos.innerHTML = "";
+    }
 
     for (let i = 0; i < 12; i++) {
         if ((isNaN(fechaCorrecta[i][0]) || isNaN(fechaCorrecta[i][1])) || (isNaN(pares[i][0]) || isNaN(pares[i][1]))) {
@@ -326,6 +827,7 @@ function ajustarAltura() {
 function corroborar() {
 
     var texto = document.getElementById("inputTexto").value;
+    var error = document.getElementById("error");
     plenos = 0;
     parciales = 0;
     errores = 0;
@@ -341,6 +843,8 @@ function corroborar() {
         }
     }
 
+
+
     const fechaCorrecta = [
         [1, 1],
         ["X", "X"],
@@ -355,6 +859,18 @@ function corroborar() {
         ["X", "X"],
         ["X", "X"]
     ];
+
+
+    const fechaSinX = fechaCorrecta.filter(item => item[0] !== "X" && item[1] !== "X");
+    if (pares.length < fechaSinX.length) {
+        error.innerHTML = "ERROR: Te faltan partidos.";
+        return;
+    } else {
+        error.innerHTML = "";
+    }
+
+
+
 
     for (let i = 0; i < 12; i++) {
         if ((isNaN(fechaCorrecta[i][0]) || isNaN(fechaCorrecta[i][1])) || (isNaN(pares[i][0]) || isNaN(pares[i][1]))) {
